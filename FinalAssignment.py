2aae77c3e31f1dfefbdd08ad048794e2d5f7f633
unknown <frankamiel.fa@gmail.com>
2023-09-22 19:50:34 +0200

    Add: Insert word documentation

--- FinalAssignment.py
+++ FinalAssignment.py
@@ -1,22 +1,26 @@
-from textblob import TextBlob
-from newspaper import Article
-import nltk
+"""
+This is a project designed to make text sentiment analysis. This is project which aims only the text data.
+"""
+
+from textblob import TextBlob # Importing Textblob
+from newspaper import Article # Importing newspaper
+import nltk # Importing NTLK
 
 nltk.download('punkt')
 
-url = 'https://www.nbcnews.com/news/world/live-blog/morocco-earthquake-live-updates-death-toll-aid-rcna104569'
-article = Article(url)
+url = 'https://www.nbcnews.com/news/world/live-blog/morocco-earthquake-live-updates-death-toll-aid-rcna104569' # Article URL
+article = Article(url) # Define article with the Artcle URL as parameter of the Article() function.
 
 # with open('test.txt', 'r') as f:
 #     text = f.read()
 
 # text = input('How do you feel about the product: ')
 
-article.download()
-article.parse()
+article.download() # Download the article
+article.parse() # Parse the article 
 article.nlp()
 
-text = article.summary
+text = article.summary # Summarize the article content. Making it easier to be analyzed
 print("", text)
 
 blob = TextBlob(text)
